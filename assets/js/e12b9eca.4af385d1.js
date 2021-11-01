859f2d64a8bf4889c430c636cf80d90597639a6a
kdy1 <kdy1997.dev@gmail.com>
2021-11-01 11:19:27 +0000

    Deploying to master from  @ 7a1b26fb086d0dc2cf47f17f5286552092c73e78 🚀

---
+++ assets/js/e12b9eca.4af385d1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[9447],{370:(e,t,s)=>{s.r(t),s.d(t,{default:()=>c});var n=s(7294),u=s(2049);const c=function(e){return n.createElement(u.Z,null,"Moved to https://play.swc.rs")}}}]);
